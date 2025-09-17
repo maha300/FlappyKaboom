3bd78128d410fd6f5c6e0074abeedfd8084616fd
maha300 <mahaalrifai@hotmail.de>
2025-09-17 11:05:05 -0400

    Added Birdcontroller script

--- Assets/_Scripts/BirdController.cs
+++ Assets/_Scripts/BirdController.cs
@@ -5,7 +5,7 @@ public class BirdController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Bird has been loaded");
     }
 
     // Update is called once per frame
