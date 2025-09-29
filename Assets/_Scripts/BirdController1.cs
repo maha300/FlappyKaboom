77c29a80808c30d28d4ed973e115c8d9d5fabcfb
maha300 <mahaalrifai@hotmail.de>
2025-09-29 15:07:00 -0400

    Added bomber and scripts

--- Assets/_Scripts/BirdController1.cs
+++ Assets/_Scripts/BirdController1.cs
@@ -3,80 +3,86 @@ using TMPro;
 
 public class BirdController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 0.5f;
-    private Vector2 initPos;
     public TMP_Text scoreText;
-   
+
+    private Vector2 initPosition;
+
+
     void Start()
     {
-        initPos = gameObject.transform.position;
-        Debug.Log("Bird has been loaded");
+        initPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Create a new vector where we modify x position
-        //of our game object
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            // Create a new vector where we modify the x position
+            // of our game object
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x + speed,
                 gameObject.transform.position.y);
-            //Assign new position vector to game object
+
+            // Assign new position vector to game object
             gameObject.transform.position = pos;
-
+                
         }
-
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
+            // Create a new vector where we modify the x position
+            // of our game object
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x - speed,
                 gameObject.transform.position.y);
-            //Assign new position vector to game object
+
+            // Assign new position vector to game object
             gameObject.transform.position = pos;
 
         }
-
         else if (Input.GetKey(KeyCode.UpArrow))
         {
+            // Create a new vector where we modify the x position
+            // of our game object
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x,
                 gameObject.transform.position.y + speed);
-            //Assign new position vector to game object
+
+            // Assign new position vector to game object
             gameObject.transform.position = pos;
 
         }
-
         else if (Input.GetKey(KeyCode.DownArrow))
         {
+            // Create a new vector where we modify the x position
+            // of our game object
             Vector2 pos = new Vector2(
                 gameObject.transform.position.x,
                 gameObject.transform.position.y - speed);
-            //Assign new position vector to game object
+
+            // Assign new position vector to game object
             gameObject.transform.position = pos;
 
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Obstacle")
         {
             
-            //On collision, subtract 25 points
+            // On collision with obstacle, subtract 25 points
             int score = int.Parse(scoreText.text);
             score = score - 25;
             scoreText.text = score.ToString();
 
-            if (score <= 0)
+            if(score <= 0)
             {
                 Die();
             }
-
-
         }
-        if (collision.gameObject.tag == "Bomb")
+        if(collision.gameObject.tag == "Bomb")
         {
 
             // On collision with bomb, subtract 10 points
@@ -90,18 +96,16 @@ public class BirdController : MonoBehaviour
                 Die();
             }
         }
-
-
     }
+
     private void Die()
     {
-        //Play Death sound
+        // Play the death scream
         gameObject.GetComponent<AudioSource>().Play();
 
-        //Make bird flip
+        // Flip the bird upside down
         gameObject.GetComponent<SpriteRenderer>().flipY = true;
 
         gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
-
     }
 }
